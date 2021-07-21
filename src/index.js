98205f7c6d7da5249efd7c5144fd115467eb32db
kwhong95 <kwhong95@kakao.com>
2021-07-21 21:43:20 +0900

    [DEV]First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from 'react-dom'
+import {ThemeProvider} from "styled-components";
+import theme from "./assets/styles/theme";
+import GlobalStyle from "./assets/styles/global";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <h2>Hello React</h2>
+      </ThemeProvider>
+    </React.StrictMode>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
